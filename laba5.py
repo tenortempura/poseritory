651ed7c77dac2f0571e07e828fc709154212dc9f
tenortempura <tenortempura@gmail.com>
2025-02-23 16:44:07 +0300

    Add files via upload

---
+++ laba5.py
@@ -0,0 +1,18 @@
+import math
+i=0
+b=[]
+a= list(range(0,1000000))
+for i in range(len(a)):
+   if a[i]<5:
+       c=math.sqrt((a[i]**2)+5)
+       if c.is_integer():
+           b.append(c)
+   else:
+     d=math.sqrt((a[i]**2)-5)
+     c=math.sqrt((a[i]**2)+5)
+     if c.is_integer():
+           b.append(c)
+     if d.is_integer():
+           b.append(d)
+
+print (b)
