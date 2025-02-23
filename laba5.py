0a1f9a9932442af6b2cab4c7ab5a9d324247ecc7
tenortempura <tenortempura@gmail.com>
2025-02-23 17:01:11 +0300

    Update laba5.py

--- laba5.py
+++ laba5.py
@@ -2,16 +2,11 @@ import math
 b=[]
 a= list(range(0,1000000))
 for i in range(len(a)):
-   if a[i]<3:
-       c=math.sqrt((a[i]**2)+5)
-       if c.is_integer():
+   c=math.sqrt((a[i]**2)+5)
+   if c.is_integer():
            b.append(c)
-   else:
-     d=math.sqrt((a[i]**2)-5)
-     c=math.sqrt((a[i]**2)+5)
-     if c.is_integer():
-           b.append(c)
-     if d.is_integer():
+   if a[i]>=3:
+       d=math.sqrt((a[i]**2)-5)
+       if d.is_integer():
            b.append(d)
-
 print (b)
